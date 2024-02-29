7da035c9d7430f42ecc35aef6dc0262d0a966f28
thekingofspace <37231416+thekingofspace@users.noreply.github.com>
2024-02-29 15:57:40 -0800

    sdafasdf

--- events/dashboardDataUpdate.js
+++ events/dashboardDataUpdate.js
@@ -8,31 +8,15 @@ module.exports = {
     DBM.Events = DBM.Events || {};
     const { Bot, Actions } = DBM;
 
-    const waitForNextCheck = (events, currentIndex, data) => {
-      if (currentIndex < events.length - 1) {
-        setTimeout(() => {
-          checkDashboardData(events, currentIndex + 1, data);
-        }, 30000);
-      }
-    };
-
-    const checkDashboardData = (events, currentIndex, data) => {
-      const event = events[currentIndex];
-      const temp = {};
-      if (event.temp) temp[event.temp] = data;
-
-      Actions.invokeEvent(event, null, temp, (result) => {
-        if (result) {
-          waitForNextCheck(events, currentIndex, data);
-        }
-      });
-    };
-
     DBM.Events.dbmDashboardDataUpdate = function dbmDashboardDataUpdate(data) {
       if (!Bot.$evts["DBM Dashboard Data Changed"]) return;
 
-      const events = Bot.$evts["DBM Dashboard Data Changed"];
-      checkDashboardData(events, 0, data);
-    };
+      for (const event of Bot.$evts["DBM Dashboard Data Changed"]) {
+        const temp = {};
+        if(event.temp) temp[event.temp] = data;
+
+        Actions.invokeEvent(event, null, temp);
+      }
+    }
   },
 };
